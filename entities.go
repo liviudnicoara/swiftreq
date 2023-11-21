db72f366b7b9b11eb0fa62914107084558ca9adc
liviudnicoara <119079662+liviudnicoara@users.noreply.github.com>
2023-11-21 10:25:56 +0200

    Update entities.go

--- entities.go
+++ entities.go
@@ -1,19 +1,24 @@
+// Package swiftreq provides common error and response structures for HTTP requests in Golang.
+
 package swiftreq
 
 import (
 	"fmt"
 )
 
+// Error represents an error that may occur during an HTTP request.
 type Error struct {
 	Message    string
 	Cause      error
 	StatusCode int
 }
 
+// Error returns a formatted error message including the original cause and status code.
 func (e *Error) Error() string {
 	return fmt.Sprintf("message: %s\n cause: %s\n statusCode: %d", e.Message, e.Cause.Error(), e.StatusCode)
 }
 
+// Response represents the result of an HTTP request.
 type Response struct {
 	Data       interface{}
 	Success    bool
