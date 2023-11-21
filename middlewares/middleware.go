592c27c6e9065de4131e94d099ef146ec41e14ef
liviudnicoara <119079662+liviudnicoara@users.noreply.github.com>
2023-11-21 10:33:02 +0200

    Update middleware.go

--- middlewares/middleware.go
+++ middlewares/middleware.go
@@ -2,6 +2,8 @@ package middlewares
 
 import "net/http"
 
+// Handler represents a function that processes an HTTP request and returns an HTTP response or an error.
 type Handler func(req *http.Request) (*http.Response, error)
 
+// Middleware represents a function that takes a Handler and returns a new Handler with additional behavior.
 type Middleware func(next Handler) Handler
